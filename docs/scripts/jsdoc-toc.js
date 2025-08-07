f7a1f3d8f885db6a50493a5410c270a0fc5e097d
jlongyam <jhonliongbox@gmail.com>
2025-08-07 11:57:17 +0700

    docs: Add documentation files generated by JSDoc

---
+++ docs/scripts/jsdoc-toc.js
@@ -0,0 +1,17 @@
+(function($) {
+    // TODO: make the node ID configurable
+    var treeNode = $('#jsdoc-toc-nav');
+
+    // initialize the tree
+    treeNode.tree({
+        autoEscape: false,
+        closedIcon: '&#x21e2;',
+        data: [{"label":"<a href=\"env.html\">env</a>","id":"env","children":[]},{"label":"<a href=\"module-utility.html\">utility</a>","id":"module:utility","children":[]}],
+        openedIcon: ' &#x21e3;',
+        saveState: false,
+        useContextMenu: false
+    });
+
+    // add event handlers
+    // TODO
+})(jQuery);
